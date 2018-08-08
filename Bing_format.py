2c13448810bcb77ddcbfa1dd1937179e3f8a5aae
Mathis Loïc Messager <messamat@uw.edu>
2018-08-08 15:13:30 -0700

    Compute bing heat map and create heat index out of 100

--- Bing_format.py
+++ Bing_format.py
@@ -14,6 +14,7 @@ arcpy.env.workspace=res
 mlclist = [filenames for (dirpath, dirnames, filenames) in walk(res)][0]
 regex=re.compile(".+mlc\.tif$")
 clasfiles = [m.group(0) for l in mlclist for m in [regex.search(l)] if m] #Look into this
+kernel = NbrWeight(path.join(rootdir, 'results/logkernel100.txt')) #UPDATE
 
 #Note that 180623_02_45 was deleted because too incomplete and 180625_22_45 is missing tiles (scheduled task crashed)
 
@@ -30,19 +31,40 @@ for f in clasfiles:
         outReclass.save(outf)
     print(time.time() - t0)
 
-#Boolean to 0-1 white
-#Focal statistics
-#Raster calculator: if mainclass=red and focal=white, then 0, otherwise, main class
-
-#Compute statistics across time (30 min for 19 rasters, expect ~4-5h for )
+#Compute statistics across time - 5h for a week of data
 mlclist = [filenames for (dirpath, dirnames, filenames) in walk(res)][0]
 regex=re.compile(".+reclass\.tif$")
-reclasfiles = [m.group(0) for l in mlclist for m in [regex.search(l)] if m] #Look into this
+reclasfiles = [m.group(0) for l in mlclist for m in [regex.search(l)] if m]
 t0 = time.time()
 arcpy.env.extent = "MAXOF"
+bingmeanout = 'bingmean1806.tif'
 bingmean = CellStatistics(reclasfiles, statistics_type='MEAN', ignore_nodata='DATA')
-bingmean.save('bingmean1806.tif')
+bingmean.save(bingmeanout)
 t1=time.time()
 print(t1-t0)
 
-#bingmean = CellStatistics(reclasfiles, statistics_type='MEAN', ignore_nodata='DATA')
+
+#Majority across time
+#Remove those that have a majority of 3
+
+#Instead, use
+bingcleanout = 'bingmeanclean1806.tif'
+bingclean = Con(Raster(bingmeanout)<2.5, Raster(bingmeanout), 0)
+bingclean.save(bingcleanout)
+
+#Generate heat map
+heat_bing = FocalStatistics(path.join(res,bingcleanout), neighborhood=kernel, statistics_type='SUM', ignore_nodata='DATA') #NEed to be in geodatabse
+heat_bing.save('heat_bing')
+heat_bing_int = Int(Raster('heat_bing')+0.5)
+heat_bing_int.save('heat_bing_int')
+
+
+#Integerize bing heat map
+arcpy.ProjectRaster_management(heat_bing, 'heat_bing_proj', UTM10, resampling_type='BILINEAR') #Project
+heat_bing_int = Int(Raster('heat_bing_proj')+0.5)
+heat_bing_int.save('heat_bing_int')
+
+#Compute a heat index out of 100 (standardized, but non-transformed -- for communication)
+bingmax = arcpy.GetRasterProperties_management('heat_bing_int', 'MAXIMUM')
+bingheatindex = 100*Float(Raster('heat_bing_int'))/float(bingmax.getOutput(0))
+bingheatindex.save('heat_bing_index2')
