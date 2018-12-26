591a2a10caf313a97f1542de8b3242bc51f69425
Mathis Loïc Messager <messamat@uw.edu>
2018-12-26 08:16:27 -0800

    Troubleshoot tiling cleaning and euclidean allocation loop and loop over kernels to produce heatmaps

--- Bing_format.py
+++ Bing_format.py
@@ -15,8 +15,7 @@ arcpy.env.workspace=res
 mlclist = [filenames for (dirpath, dirnames, filenames) in walk(res)][0]
 regex=re.compile(".+mlc\.tif$")
 clasfiles = [m.group(0) for l in mlclist for m in [regex.search(l)] if m] #Look into this
-
-kernel_log100 = NbrWeight(path.join(rootdir, 'results/logkernel100.txt')) #UPDATE
+UTM10 = arcpy.SpatialReference(26910)
 
 #Bing logo masks
 logomask_odd = path.join(rootdir, 'results/boolean_logo.tif')
@@ -76,70 +75,87 @@ print(t1-t0)
 
 #Compute mean for all days at 3 am to identify semi-permanent road closures
 if not arcpy.Exists(traffic3am_ras):
+    arcpy.env.extent = "MAXOF"
+    print('Produce 3 am traffic layer')
     traffic3am_raslist = arcpy.ListRasters('reclas_*_03_00')
     traffic3am_mean  = CellStatistics(traffic3am_raslist, statistics_type='MEAN', ignore_nodata='DATA')
     traffic3am_mean.save(traffic3am_ras)
 if not arcpy.Exists(traffic2am_ras):
+    arcpy.env.extent = "MAXOF"
+    print('Produce 2 am traffic layer')
     traffic2am_raslist = arcpy.ListRasters('reclas_*_02_00')
     traffic2am_mean  = CellStatistics(traffic2am_raslist, statistics_type='MEAN', ignore_nodata='DATA')
     traffic2am_mean.save(traffic2am_ras)
 
-#Clean out bing logo and road closure artefacts
-#In arcmap: Int(Con(("bingmeanod"<1) & IsNull("boolean_logo.tif") & ("traffic3am"<0.2), 1000*"bingmeanod"))
+#Clean out bing logo and road closure artefacts then run euclidean allocation on tiles
 arcpy.ResetEnvironments()
-totalextent = arcpy.Describe(bingmean_odd).Extent
-n_2d = 5 #Number of side tiles (total number of tiles = n_2d^2)
-xpl = range(int(totalextent.XMin), int(math.ceil(totalextent.XMax)), int(math.ceil((totalextent.XMax - totalextent.XMin)/n_2d))) + [int(math.ceil(totalextent.XMax))]
-ypl = range(int(totalextent.YMin), int(math.ceil(totalextent.YMax)), int(math.ceil((totalextent.YMax - totalextent.YMin)/n_2d))) + [int(math.ceil(totalextent.YMax))]
+ext_template = bingmean_odd
+totalextent = arcpy.Describe(ext_template).Extent
+n_2d = 20 #Number of side tiles (total number of tiles = n_2d^2)
+xpl = range(int(totalextent.XMin), int(math.ceil(totalextent.XMax)), int(math.ceil((totalextent.XMax - totalextent.XMin)/n_2d))+1) + [int(math.ceil(totalextent.XMax))]
+ypl = range(int(totalextent.YMin), int(math.ceil(totalextent.YMax)), int(math.ceil((totalextent.YMax - totalextent.YMin)/n_2d))+1) + [int(math.ceil(totalextent.YMax))]
 x=0
 for xmin, xmax in zip(xpl[:-1], xpl[1:]):
     for ymin, ymax in zip(ypl[:-1], ypl[1:]):
         tilext = arcpy.Extent(xmin, ymin, xmax, ymax)
         print(tilext)
         arcpy.env.extent = tilext
-        arcpy.env.snapRaster = bingmean_odd
-
+        arcpy.env.snapRaster = ext_template
         #Odd hour tile processing
-        outodd = bingclean_odd + str(x)
-        if not arcpy.Exists(outodd):
-            print(outodd)
-            bingclean = Int(Con((Raster(bingmean_odd)<1.0) & IsNull(Raster(logomask_odd)) & (Raster(traffic3am_ras)<0.2),
-                        1000*Raster(bingmean_odd)))
-            bingclean.save(outodd)
-            arcpy.Delete_management(bingclean)
+        outeuc = bingeuc + str(x)
 
-        #Even hour tile processing
-        outeven = bingclean_even + str(x)
-        if not arcpy.Exists(outeven):
-            print(outeven)
-            bingclean = Int(Con((Raster(bingmean_even)<1.0) & IsNull(Raster(logomask_even)) & (Raster(traffic2am_ras)<0.2),
-                        1000*Raster(bingmean_even)))
-            bingclean.save(outeven)
-            arcpy.Delete_management(bingclean)
+        try:
+            arcpy.Delete_management(clean_mean)
+        except:
+            pass
+        try:
+            arcpy.Delete_management(euc)
+        except:
+            pass
+
+        if not arcpy.Exists(outeuc):
+            print(outeuc)
+            clean_mean = Int(1000*
+                             Con((Raster(bingmean_odd)<1.0) & IsNull(Raster(logomask_odd)) & (Raster(traffic3am_ras)<0.2) &
+                             (Raster(bingmean_even)<1.0) & IsNull(Raster(logomask_even)) & (Raster(traffic2am_ras)<0.2),
+                             (Raster(bingmean_odd)+Raster(bingmean_even))/2,
+                             Con((Raster(bingmean_odd)<1.0) & IsNull(Raster(logomask_odd)) & (Raster(traffic3am_ras)<0.2),
+                                 Raster(bingmean_odd),
+                                 Con((Raster(bingmean_even)<1.0) & IsNull(Raster(logomask_even)) & (Raster(traffic2am_ras)<0.2),
+                                     Raster(bingmean_even)
+                                     )
+                                 )
+                             ))
+            print('Done cleaning and averaging, running euclidean allocation')
+            euc = arcpy.sa.EucAllocation(clean_mean, maximum_distance=50)
+            euc.save(bingeuc)
+            arcpy.Rename_management(bingeuc, outeuc)
         x+=1
 
-#Average over odd and even hours (mosaic to new raster with mean)
+#Mosaic all tiles
 arcpy.ResetEnvironments()
-arcpy.env.workspace = res
-arcpy.MosaicToNewRaster_management(arcpy.ListRasters('bingclean*'), gdb, 'bingcleanmea', number_of_bands= 1,
-                                   pixel_type = '16_BIT_UNSIGNED', mosaic_method='MEAN')
-#Euclidean allocation
-#Create raster of NoData value to set extent
-bingeuc = path.join(gdb, 'bingeuc')
-euc = arcpy.sa.EucAllocation(bingclean_mean, maximum_distance=50)
-euc.save(bingeuc)
+if not arcpy.Exists(path.join(gdb, 'bingeuc')):
+    arcpy.env.workspace = res
+    arcpy.MosaicToNewRaster_management(arcpy.ListRasters('bingeuc*'), gdb, 'bingeuc', coordinate_system_for_the_raster= UTM10,
+                                       pixel_type = '16_BIT_UNSIGNED', number_of_bands=1, mosaic_method='LAST')
 
-#Generate heat map for multiple kernel types (create for loops)
-heat_bing = FocalStatistics(path.join(gdb, bingclean_mean), neighborhood=kernel_log100, statistics_type='SUM', ignore_nodata='DATA') #NEed to be in geodatabse
-heat_bing.save('heat_bing')
-heat_bing_int = Int(Raster('heat_bing')+0.5)
-heat_bing_int.save('heat_bing_int')
+#Create heatmap - focal statistics
+keyw = '_pow.*(?<=2)[.]txt'
+#keyw = '_log'
+for kertxt in listdir(path.join(rootdir, 'results/bing')):
+    if re.compile('kernel'+keyw).match(kertxt):
+        outext = '_bingheat_bing{}'.format(path.splitext(kertxt)[0][7:])
+        if not arcpy.Exists(path.join(gdb,outext)):
+            print(outext)
+            tic = time.time()
+            kernel = NbrWeight(path.join(rootdir, 'results/bing', kertxt))
+            heat_bing = Int(arcpy.sa.FocalStatistics(path.join(gdb, 'bingeuc'), neighborhood= kernel, statistics_type='SUM', ignore_nodata='DATA')/100+0.5)
+            heat_bing.save(path.join(gdb,outext))
+            toc = time.time()
+            print(toc-tic)
 
-#Integerize bing heat map, project, and compute heat index out of 100
-arcpy.ProjectRaster_management(heat_bing, 'heat_bing_proj', UTM10, resampling_type='BILINEAR') #Project
-heat_bing_int = Int(Raster('heat_bing_proj')+0.5)
-heat_bing_int.save('heat_bing_int')
-
+#Project, and compute heat index out of 100
+arcpy.ProjectRaster_management(path.join(gdb, 'bingeuc'), 'bingeuc_proj', UTM10, resampling_type='BILINEAR') #Project
 #Compute a heat index out of 100 (standardized, but non-transformed -- for communication)
 bingmax = arcpy.GetRasterProperties_management('heat_bing_int', 'MAXIMUM')
 bingheatindex = 100*Float(Raster('heat_bing_int'))/float(bingmax.getOutput(0))
