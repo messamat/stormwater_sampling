f2db3d5b53418851e21400015558530062ce9190
Mathis Loïc Messager <messamat@uw.edu>
2018-12-28 11:43:56 -0800

    Include calendar_dates.txt data in weekly trip count + clarify code structure

--- GTFStoSHP.py
+++ GTFStoSHP.py
@@ -26,6 +26,7 @@ import arcpy
 import os
 import zipfile
 import logging
+from collections import defaultdict
 from datetime import datetime
 
 arcpy.env.overwriteOutput=True
@@ -65,18 +66,23 @@ def GTFStoSHPweeklynumber(gtfs_dir, out_gdb, out_fc, current=True, keep=False):
     print('trips.txt imported')
     arcpy.CopyRows_management(os.path.join(gtfs_dir,'calendar.txt'), 'calendar')
     print('calendar.txt imported')
+    # arcpy.CopyRows_management(os.path.join(gtfs_dir, 'calendar_dates.txt'), 'calendar') #Imports in wrong format
+    # print('calendar_dates.txt imported')
     #arcpy.CopyRows_management(os.path.join(gtfs_dir,'stop_times.txt'), 'stop_times')
 
-    #Create shapefile from shapes.txt
+    #-------------------------------------------------------------------------------------------------------------------
+    # CREATE SHAPEFILE FROM SHAPES.TXT
+    #-------------------------------------------------------------------------------------------------------------------
     print('Creating point feature class...')
+
     newfc = arcpy.CreateFeatureclass_management(out_gdb, out_fc, "Point")
     arcpy.DefineProjection_management(newfc , 4326)
     
-    txt = open(os.path.join(gtfs_dir, 'shapes.txt'), 'r')
-    inputF = txt.readlines()
+    shp_txt = open(os.path.join(gtfs_dir, 'shapes.txt'), 'r')
+    shp_F = shp_txt.readlines()
 
     #Create columns based on those in shape.txt
-    headers = inputF[0].replace('\n', '').split(',')
+    headers = shp_F[0].replace('\n', '').split(',')
     arcpy.AddField_management(newfc, 'shape_id', 'TEXT', field_length = 50)
     arcpy.AddField_management(newfc, 'shape_pt_lat', 'FLOAT')
     arcpy.AddField_management(newfc, 'shape_pt_lon', 'FLOAT')
@@ -90,7 +96,7 @@ def GTFStoSHPweeklynumber(gtfs_dir, out_gdb, out_fc, current=True, keep=False):
 
     #Fill in point feature class
     with arcpy.da.InsertCursor(newfc, headers + ["SHAPE@XY"]) as cursor:
-        for line in inputF[1:]:
+        for line in shp_F[1:]:
             try:
                 lineformat = line.replace('\n', '').split(',')
                 lineformat.append((float(lineformat[ix]), float(lineformat[iy]))) #Add coordinate tuple
@@ -98,26 +104,68 @@ def GTFStoSHPweeklynumber(gtfs_dir, out_gdb, out_fc, current=True, keep=False):
             except Exception as e:
                 print(e)
                 logging.warning('{0} \n Error row: {1}'.format(e, line))
+    del shp_txt
+    del shp_F
 
     #Convert to polyline
     print('Converting points to lines...')
     newfc_lines = out_fc + '_lines'
     arcpy.PointsToLine_management(newfc, newfc_lines, Line_Field= 'shape_id')
 
-    #Each route has multiple trips
-    #Eacj trip is a unique bus line-time-week day combination (e.g. 6:47 am MON-FRI Line 71)
-    #Compute number of days/week for each service_id in calendar
+    #-------------------------------------------------------------------------------------------------------------------
+    # COMPUTE NUMBER OF WEEKLY TRIPS PER SHAPE
+    #-------------------------------------------------------------------------------------------------------------------
     print('Summaryzing data...')
-    arcpy.AddField_management('calendar', 'daynum', 'SHORT')
-    cal_fields = ['daynum', 'start_date', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+    #Analyze calendar dates for trips with no calendar schedule
+    '''
+    In some cases, calendar.txt was entirely omitted for a trip, and ALL dates of service were included in 
+    calendar_dates.txt (https://developers.google.com/transit/gtfs/reference/#calendar_datestxt).
+    This happens when schedule varies most days of the month, or the agency wants to programmatically output service 
+    dates without specifying a normal weekly schedule. In most cases however, calendar_dates.txt is used to define 
+    exceptions to the default service categories defined in calendar.txt file. 
+    '''
+    cal_txt = open(os.path.join(gtfs_dir, 'calendar_dates.txt'), 'r')
+    cal_F = cal_txt.readlines()
+    #cal_headers = cal_F[0].replace('\n', '').split(',')
+    # Create a dictionary to store number of dates for each exception_type: 1 service added, 2 service removed
+    cal_dic = defaultdict(lambda: [0]*2)
+    for line in cal_F[1:]:
+        lineformat = line.replace('\n', '').split(',')
+        cal_dic[lineformat[0]][int(lineformat[2])-1] += 1
+
+    #Each route has multiple trips
+    #Each trip is a unique bus line-time-week day combination (e.g. 6:47 am MON-FRI Line 71)
+    #Compute number of days/week for each service_id in calendar
+    arcpy.AddField_management('calendar', 'normalnum', 'SHORT')
+    arcpy.AddField_management('calendar', 'date_added', 'SHORT')
+    arcpy.AddField_management('calendar', 'date_removed', 'SHORT')
+    arcpy.AddField_management('calendar', 'date_weekavg', 'FLOAT')
+    arcpy.AddField_management('calendar', 'adjustnum', 'FLOAT')
+
+    cal_fields = ['service_id',  'start_date','end_date', 'normalnum', #0, 1, 2, 3
+                  'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', #4-10
+                  'date_added', 'date_removed', 'date_weekavg', 'adjustnum'] #11, 12, 13, 14
+
     with arcpy.da.UpdateCursor('calendar', cal_fields) as cursor:
         for row in cursor:
-            #Make sure that start_date of trip is < present
-            if datetime.strptime(str(row[1]), '%Y%m%d') < datetime.now():
-                row[0] = sum(row[2:])
+            print(row[1])
+            if row[0] in cal_dic: #if service_id in calendar_dates dictionary
+                row[11] = cal_dic[row[0]][1] #date_added = number of added service dates in calendar_dates.txt (type 1)
+                row[12] = cal_dic[row[0]][2] #date_removed = number of removed service dates in calendar_dates.txt (type 2)
+                service_len = (datetime.strptime(str(row[2]), '%Y%m%d') -
+                               datetime.strptime(str(row[1]), '%Y%m%d')).days / 7.0 #Compute length of service in weeks
+                row[13] = (cal_dic[row[0]][1]-cal_dic[row[0]][2])/service_len #Compute average net number of exceptions/week
             else:
-                if current == True:
-                    row[0] = 0
+                row[11:14] = (0,0,0)
+            cursor.updateRow(row) #update row halfway to avoid having to retype row[13] equation
+
+            row[3] = sum(row[4:11]) #normalnum = sum(monday-sunday trips)
+            row[14] = sum(row[4:11]) + row[13] #adjustnum = normalnum + date_weekavg
+
+            if datetime.strptime(str(row[1]), '%Y%m%d') > datetime.now() and current == True:  # If start_date > current date
+                row[3] = 0
+                row[14] = 0
             cursor.updateRow(row)
 
     #Join calendar to trips
@@ -126,11 +174,18 @@ def GTFStoSHPweeklynumber(gtfs_dir, out_gdb, out_fc, current=True, keep=False):
     arcpy.AddJoin_management('trips_view', 'route_id', 'routes', 'route_id', join_type='KEEP_ALL')
     arcpy.CopyRows_management('trips_view', 'trips_routes_calendar')
     
-    #Summary trips by route_id & shape_id (here add
-    arcpy.Statistics_analysis('trips_routes_calendar', 'trips_routes_count', statistics_fields=[['daynum', 'SUM']],
-                              case_field = ['shape_id', 'route_id', 'route_short_name', 'route_desc', 'route_type'])
+    #Summarize trips by route_id & shape_id (here add
+    arcpy.Statistics_analysis('trips_routes_calendar', 'trips_routes_count',
+                              statistics_fields=[['normalnum', 'SUM'], ['adjustnum', 'SUM'],
+                                                 ['direction_id', 'FIRST'], ['direction_id', 'LAST']],
+                              case_field = ['shape_id', 'route_id', 'route_short_name',
+                                            'route_desc', 'route_type'])
 
+    #-------------------------------------------------------------------------------------------------------------------
+    # JOIN AND EXPORT DATE
+    #-------------------------------------------------------------------------------------------------------------------
     print('Joining and exporting data...')
+
     #Make sure that shape_id in join table is in text form
     for f in arcpy.ListFields('trips_routes_count'):
         if f.name == 'shape_id':
