acef3b1d26182a6d4b4cc6028253ec144bb91b81
Mathis Loïc Messager <messamat@uw.edu>
2019-03-15 08:45:53 -0700

    Format USDOT highway AADT data, start formatting state VMT tables

--- USwide_OSMformat.py
+++ USwide_OSMformat.py
@@ -7,13 +7,21 @@ import itertools
 import shapely
 from BeautifulSoup import BeautifulSoup
 import urllib2
+import zipfile
+from xlrd import open_workbook
+from xlutils.copy import copy
+
+pd.options.display.max_columns = 20
 
 rootdir = "D:\Mathis\ICSL\stormwater"
 USDOTdir = os.path.join(rootdir, "data\USDOT_0319")
 resdir = os.path.join(rootdir, 'results/usdot')
 
+#Output variables
+ushpms = os.path.join(resdir, 'ushpms')
+
 ########################################################################################################################
-# FORMAT TMAS DATA 
+# FORMAT TMAS DATA
 ########################################################################################################################
 exampletab = os.path.join(USDOTdir, "dec_2017_ccs_data\\12. DEC\\AK_DEC_2017 (TMAS).VOL")
 
@@ -205,13 +213,111 @@ len(list(set(dec2017_all['stationID'])))
 ########################################################################################################################
 # FORMAT HIGHWAY AADT DATA
 ########################################################################################################################
+#Function to download file
+def dlfile(url, outpath):
+    # Open the url
+    try:
+        f = urllib2.urlopen(url)
+        print "downloading " + url
+
+        # Open our local file for writing
+        with open(outpath, "wb") as local_file:
+            local_file.write(f.read())
+
+    #handle errors
+    except urllib2.HTTPError, e:
+        print "HTTP Error:", e.code, url
+    except urllib2.URLError, e:
+        print "URL Error:", e.reason, url
+
+    #Unzip folder
+    if zipfile.is_zipfile(outpath):
+        print('Unzipping {}...'.format(os.path.split(outpath)[1]))
+        with zipfile.ZipFile(outpath) as zipf:
+            zipfilelist = [info.filename for info in zipf.infolist()]
+            listcheck = [f for f in zipfilelist if os.path.exists(os.path.join(outpath, f))]
+            if len(listcheck) > 0:
+                print('Overwriting {}...'.format(', '.join(listcheck)))
+            zipf.extractall(os.path.split(outpath)[0])
+        del zipf
+
+
 #Get HPMS data for every state
-"https://www.fhwa.dot.gov/policyinformation/hpms/shapefiles.cfm"
+hpmsurl = "https://www.fhwa.dot.gov/policyinformation/hpms/shapefiles.cfm"
+hpms_page = urllib2.urlopen(hpmsurl)
+hpms_soup = BeautifulSoup(hpms_page)
+hpmsshp = []
+hpms_regex = re.compile(".*hpms[/]shapefiles[/](?!nationalarterial)[a-z]+2017[.]zip")
+for link in hpms_soup.findAll('a', attrs={'href': hpms_regex}):
+    zipurl = os.path.split(hpmsurl)[0] + '/' + \
+             '/'.join([i for i in link.get('href').split('/') if i not in os.path.split(hpmsurl)[0].split('/')])
+    outpath = os.path.join(USDOTdir, 'HPMS', os.path.split(zipurl)[1])
 
+    if outpath not in hpmsshp:
+        hpmsshp.append('{}.shp'.format(os.path.splitext(outpath)[0]))
 
-#Merge it all
-#Merge by spatial overlap
+    if not os.path.exists(outpath):
+        dlfile(zipurl, outpath)
+    else:
+        print('{} already exists...'.format(outpath))
+
+#Merge all HPMS data
+ushpms = os.path.join(resdir, 'ushpms')
+print('{} missing shapefiles...'.format(len([file for file in hpmsshp if not os.path.exists(file)])))
+arcpy.Merge_management([file for file in hpmsshp if os.path.exists(file)], ushpms)
+
+#Merge with OSM by spatial overlap
+
 #Get VM-2 estimates of Vehicle Miles Traveled for each state and average it for small roads at state level (idea fromhttps://www.pnas.org/content/pnas/suppl/2015/04/01/1421723112.DCSupplemental/pnas.1421723112.sapp.pdf)
 #Highway Statistics Book 2017
 # Public road lenth 2017 - miles by functional system, Table HM-20: https://www.fhwa.dot.gov/policyinformation/statistics/2017/hm20.cfm
+hm20url = "https://www.fhwa.dot.gov/policyinformation/statistics/2017/hm20.cfm"
+hm20_page = urllib2.urlopen(hm20url)
+hm20_soup = BeautifulSoup(hm20_page)
+hm20_regex = re.compile(".*hm20.*[.]xls")
+hm20urltab = os.path.split(hm20url)[0] + '/' + \
+             '/'.join([i for i in hm20_soup.find('a', attrs={'href': hm20_regex}).get('href').split('/')
+                       if i not in os.path.split(hm20url)[0].split('/')])
+hm20tab = os.path.join(USDOTdir, os.path.split(hm20urltab)[1])
+dlfile(hm20urltab, hm20tab)
+
 # Functional system travel 2017 - Annual vehicle-miles, Table VM-2: https://www.fhwa.dot.gov/policyinformation/statistics/2017/vm2.cfm
+vm2url = "https://www.fhwa.dot.gov/policyinformation/statistics/2017/vm2.cfm"
+vm2_page = urllib2.urlopen(vm2url)
+vm2_soup = BeautifulSoup(vm2_page)
+vm2_regex = re.compile(".*vm2.*[.]xls")
+vm2urltab = os.path.split(vm2url)[0] + '/' + \
+             '/'.join([i for i in vm2_soup.find('a', attrs={'href': vm2_regex}).get('href').split('/')
+                       if i not in os.path.split(vm2url)[0].split('/')])
+vm2tab = os.path.join(USDOTdir, os.path.split(vm2urltab)[1])
+dlfile(vm2urltab, vm2tab)
+
+#Format tables
+hm20pd = pd.ExcelFile(hm20tab)
+hm20sheet = hm20pd.parse(hm20pd.sheet_names[1])
+#Format header row
+hm20sheet_tab = hm20sheet.loc[8:,:] #Remove title
+hm20sheet_tab.loc[8,:][1:9] = hm20sheet_tab.loc[8,:][1] #Extend rural across columns
+hm20sheet_tab.loc[8,:][9:17] = hm20sheet_tab.loc[8,:][9] #Extend urban across colums
+hm20sheet_format = hm20sheet.loc[12:,:].reset_index(drop=True)
+hm20sheet_format.columns = hm20sheet_tab.loc[8:11,:].apply(lambda col: col.str.cat(), axis=0)
+#Drop totals
+hm20sheet_format = hm20sheet_format.drop(axis=1, labels = [col for col in hm20sheet_format.columns if re.compile('.*TOTAL$').match(col)])
+hm20sheet_format = hm20sheet_format.loc[hm20sheet_format.STATE.str.contains('.*(?<!\sTotal)$'),:]
+
+
+
+
+
+
+
+
+
+#Delete rows 1-11 and 66-
+#Delete columns I, Q, R-
+#Merge rows 12-14 for each column
+#
+
+
+
+
